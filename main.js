53fd9412b8900f3118f16e4fe1b52f9e373248d0
RobertAKARobin <robertgfthomas@gmail.com>
2017-06-02 15:24:20 -0500

    Added background color change

---
+++ main.js
@@ -0,0 +1,5 @@
+function setColor(){
+	var colorInput = document.querySelector('#color');
+	var background = document.querySelector('html');
+	background.style.backgroundColor = colorInput.value;
+}
